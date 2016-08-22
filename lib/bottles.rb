d1fa1a8d79229eb0bc3387eba2bedb77c4f9c0ed
Hemal <h.n.varambhia@gmail.com>
2016-08-22 13:21:13 +0100

    We can handle the case where there are no bottles of beer on the wall

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,10 @@
 class Bottles
   def verse number
-    subject = number == 1 ? 'it' : 'one'
-    "#{bottles_of_beer number} on the wall, #{bottles_of_beer number}.\n" +
-    "Take #{subject} down and pass it around, " +  
-    "#{bottles_of_beer number - 1} on the wall.\n"
+    [ 
+      "#{bottles_of_beer(number).capitalize} on the wall, #{bottles_of_beer number}.\n",
+      line_two(number),
+      line_three(number)
+    ].join
   end
 
   private
@@ -11,7 +12,26 @@ class Bottles
   def bottles_of_beer number
     return "#{number} bottle of beer" if number == 1
     return 'no more bottles of beer' if number == 0
+    return '99 bottles of beer' if number < 0
 
    "#{number} bottles of beer"
+  end
+
+  def line_two number
+    subject = number == 1 ? 'it' : 'one'
+    line_two = 
+      if number == 0
+        'Go to the store and buy some more, 99 bottles of beer on the wall.'
+      else
+        "Take #{subject} down and pass it around, "
+      end
+  end
+
+  def line_three number
+    if number >= 1
+      "#{bottles_of_beer number - 1} on the wall.\n"
+    else
+      "\n"
+    end
   end
 end
