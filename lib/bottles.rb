5f4ccb1098dd0f4b540a65d3d3873fece297bdb2
Hemal <h.n.varambhia@gmail.com>
2016-08-25 13:02:48 +0100

    Extracted a verse class, where the number is its state

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,41 +7,51 @@ class Bottles
     from = [start, finish].min
     to = [start, finish].max
 
-    (from..to).map { |number| verse number }.reverse.join("\n")
+    verses = 
+      (from..to).map { |number| verse number }.reverse.join "\n"
   end
 
   def verse number
-    [ 
-      line_one(number),
-      line_two(number),
-      line_three
-    ].join("\n")
+    Verse.new(number).to_s
   end
 
   private
-  
-  def bottles_of_beer number
-    return "#{number} bottle of beer" if number == 1
-    return 'no more bottles of beer' if number == 0
-    return '99 bottles of beer' if number < 0
 
-   "#{number} bottles of beer"
-  end
-
-  def line_one number
-    "#{bottles_of_beer(number).capitalize} on the wall, #{bottles_of_beer number}."
-  end
-
-  def line_two number
-    if number == 0
-      return "Go to the store and buy some more, #{bottles_of_beer 99} on the wall."
+  class Verse
+    def initialize number
+      @number = number
+      @lines = [ line_one, line_two, line_three ]
     end
 
-    subject = number == 1 ? 'it' : 'one'
-    "Take #{subject} down and pass it around, #{bottles_of_beer number - 1} on the wall."
-  end
+    def to_s
+      @lines.join "\n"
+    end
 
-  def line_three
-    ''
+    private
+  
+    def bottles_of_beer number
+      return "#{number} bottle of beer" if number == 1
+      return 'no more bottles of beer' if number == 0
+      return '99 bottles of beer' if number < 0
+
+      "#{number} bottles of beer"
+    end
+
+    def line_one
+      "#{bottles_of_beer(@number).capitalize} on the wall, #{bottles_of_beer @number}."
+    end
+
+    def line_two
+      if @number == 0
+        return "Go to the store and buy some more, #{bottles_of_beer 99} on the wall."
+      end
+
+      subject = @number == 1 ? 'it' : 'one'
+      "Take #{subject} down and pass it around, #{bottles_of_beer @number - 1} on the wall."
+    end
+
+    def line_three
+      ''
+    end
   end
 end
