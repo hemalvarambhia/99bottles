8aff0af1a42bd34553c774ffe295f49f784eec76
Hemal <h.n.varambhia@gmail.com>
2016-09-06 13:09:03 +0100

    There is no justification for the class. It makes the code more complex

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,51 +11,36 @@ class Bottles
   end
 
   def verse number
-    Verse.new(number).to_s
+    [line_one(number), line_two(number), line_three].join("\n")
   end
 
   private
 
-  class Verse
-    def initialize number
-      @number_of_bottles = number
+  def line_one number
+    "#{bottles_of_beer(number).capitalize} on the wall, " +
+      "#{bottles_of_beer number}."
+  end
+
+  def line_two number
+    if number == 0
+      return "Go to the store and buy some more, " + 
+        "#{bottles_of_beer 99} on the wall."
     end
 
-    def to_s
-      [ line_one, line_two, line_three ].join "\n"
-    end
+    subject = number == 1 ? 'it' : 'one'
+    "Take #{subject} down and pass it around, " +
+      "#{bottles_of_beer number - 1} on the wall."
+  end
 
-    private
+  def line_three
+    ''
+  end
 
-    def line_one
-      "#{bottles_of_beer(@number_of_bottles).capitalize} on the wall, " +
-        "#{bottles_of_beer @number_of_bottles}."
-    end
+  def bottles_of_beer number
+    bottles = "#{number} bottles" if number > 1
+    bottles =  "1 bottle" if number == 1
+    bottles =  'no more bottles' if number == 0
 
-    def line_two
-      if @number_of_bottles == 0
-        return "Go to the store and buy some more, " + 
-          "#{bottles_of_beer 99} on the wall."
-      end
-
-      "Take #{subject} down and pass it around, " +
-        "#{bottles_of_beer @number_of_bottles - 1} on the wall."
-    end
-
-    def line_three
-      ''
-    end
-
-    def subject
-      @number_of_bottles == 1 ? 'it' : 'one'
-    end
-
-    def bottles_of_beer number
-      bottles = "#{number} bottles" if number > 1
-      bottles =  "1 bottle" if number == 1
-      bottles =  'no more bottles' if number == 0
-
-      "#{bottles} of beer"
-    end
+    "#{bottles} of beer"
   end
 end
